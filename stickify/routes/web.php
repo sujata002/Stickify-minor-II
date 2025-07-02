7deff3b86769a14241009b0f25b9372c7f3cacca
Sujata Karki <sujata.karki023@apexcollege.edu.np>
2025-07-02 17:50:42 +0545

    set up login page UI with Blade, added CSS and images

--- stickify/routes/web.php
+++ stickify/routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;            // importing this since we have used LoginController tala route ma
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//giving route for login.blade.php page ani teslai name deko euta account.login bhanera
+Route::get('/login',[LoginController::class,'index'])->name('account.login');
