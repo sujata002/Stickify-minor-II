2fcb1ab7383e14242c5b23969e79e21b6b7757ab
deevyagtm <deevyagtm@gmail.com>
2025-07-03 12:42:05 +0545

    test

--- stickify/database/migrations/0001_01_01_000000_create_users_table.php
+++ stickify/database/migrations/0001_01_01_000000_create_users_table.php
@@ -45,6 +45,7 @@ return new class extends Migration
     {
         Schema::dropIfExists('users');
         Schema::dropIfExists('password_reset_tokens');
-        Schema::dropIfExists('sessions');
+        Schema::dropIfExists('sessions');Schema::dropIfExists('sessions');
+
     }
 };
