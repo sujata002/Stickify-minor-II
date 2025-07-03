ce792fcd0a276aa1c219a1df92cbfb99286c4957
deevyagtm <deevyagtm@gmail.com>
2025-07-03 21:16:32 +0545

    added feature

--- stickify/database/migrations/0001_01_01_000000_create_users_table.php
+++ stickify/database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,11 +12,12 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
+          $table->id(); // primary key which is auto incremented withi
           $table->string('name');
-          $table->id(); // primary key which is auto incremented within
           $table->string('email')->unique();
           $table->string('password');
           $table->timestamps();
+          $table->rememberToken();
         });
     }
 
