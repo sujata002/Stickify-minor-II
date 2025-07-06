301e0dbbe9ecbdff55bc485a34b463b704e4eac6
deevyagtm <deevyagtm@gmail.com>
2025-07-06 21:13:31 +0545

    JSON function and update delete changes

--- stickify/app/Http/Controllers/NotesController.php
+++ stickify/app/Http/Controllers/NotesController.php
@@ -7,14 +7,14 @@ use App\Models\Note;
 
 class NotesController extends Controller
 {
-    // List all notes for the authenticated user
+    // harek user ko overall notes haru lai list garne 
     public function index(Request $request)
     {
         $notes = $request->user()->notes()->get();
         return response()->json($notes);
     }
 
-    // Create a new note
+    // naya note creation part 
     public function store(Request $request)
     {
         $request->validate([
@@ -30,22 +30,25 @@ class NotesController extends Controller
         return response()->json($note, 201);
     }
 
-    // Show a single note by ID
+    //  note ko detail dekhaune through id 
     public function show(Request $request, $id)
     {
         $note = $request->user()->notes()->findOrFail($id);
+        //sends the note back to user ko browswer 
         return response()->json($note);
     }
 
-    // Update a note by ID
+    // id ko through note update garne 
     public function update(Request $request, $id)
     {
         $request->validate([
             'note_text' => 'required|string',
             'url' => 'nullable|string',
         ]);
-
+        // gets currently logged in vako user and accesses user ko related notes through models relationship
+        //if note exists note is given back or error throw garcha 404 not found 
         $note = $request->user()->notes()->findOrFail($id);
+        // note update huncha if any changes made 
         $note->update([
             'note_text' => $request->note_text,
             'url' => $request->url,
@@ -54,12 +57,16 @@ class NotesController extends Controller
         return response()->json($note);
     }
 
-    // Delete a note by ID
+    // id ko through note delete garne 
     public function destroy(Request $request, $id)
     {
         $note = $request->user()->notes()->findOrFail($id);
         $note->delete();
 
         return response()->json(['message' => 'Note deleted successfully']);
+        // JSON(Java Script Object Notation) is a lightweight text or simple text jun chai
+        // server dekhi client ko broswer ma jancha so client can easily use that data 
+        // in this case user lai chai note succesfully delete bhayo vanera message jancha,communicate garne medium ho user sanga 
+        
     }
 }
