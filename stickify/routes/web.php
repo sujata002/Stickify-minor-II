30ff32d62b54579794702a6ac62cf91d4feea851
Sameera Ban <sameera.ban023@apexcollege.edu.np>
2025-07-11 22:49:22 +0545

    deleted settings and profile page and instead created "modal" for pop up in the same page instead of going to another page. aldded different buttons for both profile and settings (profile needs to be modified more). also edited some of the elements via css.

--- stickify/routes/web.php
+++ stickify/routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\LoginController;            // importing this since we have used LoginController tala route ma
+use App\Http\Controllers\DashboardController;  
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+//giving route for login.blade.php page ani teslai name deko euta account.login bhanera
+
+Route::get('/account/login',[LoginController::class,'index'])->name('login');
+
+Route::get('/account/register',[LoginController::class,'register'])->name('register');
+
+Route::post('/account/process-register',[LoginController::class,'processRegister'])->name('processRegister');
+
+Route::post('/account/authenticate',[LoginController::class,'authenticate'])->name('authenticate');
+
+Route::get('/account/dashboard',[DashboardController::class,'dashboard'])->middleware('auth')->name('user.dashboard');    
+
+// laravel’s built-in Auth system and middleware do all of this when Auth::attempt() is called and protect routes with auth middleware.
+
