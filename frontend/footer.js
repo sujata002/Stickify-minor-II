66f8f174a152ea9d7d9be4841e89a0de32d1442f
Sameera Ban <sameera.ban023@apexcollege.edu.np>
2025-07-22 11:12:49 +0545

    changed things

--- frontend/footer.js
+++ frontend/footer.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function () {
   const year = new Date().getFullYear();
   document.getElementById("footer").innerHTML = `
-    <footer style="background:grey; color:white; padding:2em 1em; text-align:center; font-family:sans-serif;">
+    <footer style="background:grey; color:white; padding:1em 1em; text-align:center; font-family:sans-serif;margin-top:2em;">
       <div style="margin-bottom:1em;">
         <a href="index.html" style="color:#fff; margin:0 1em; text-decoration:none;">Home</a>
         <a href="about.html" style="color:#fff; margin:0 1em; text-decoration:none;">About</a>
@@ -17,7 +17,7 @@ document.addEventListener("DOMContentLoaded", function () {
             <a href="https://www.instagram.com" target="_blank" style="color:#fff; margin:0 0.5em; text-decoration:none;">Instagram</a>
             <a href="https://www.linkedin.com" target="_blank" style="color:#fff; margin:0 0.5em; text-decoration:none;">LinkedIn</a>
         </div>
-      <p style="margin:0.5em 0;">Email: <a href="mailto:stickify123@business.com" style="color:#fff;">stickify.123@business.com</a></p>
+      <p style="margin:0.5em 0;">Email: <a href="mailto:stickifyapp@gmail.com" style="color:#fff;">stickifyapp@gmail.com</a></p>
       <p style="margin:0.5em 0;">&copy; ©${year} Stickify. All rights reserved.</p>
     </footer>
   `;
