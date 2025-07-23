3e412a2df1db6c8e3bc4f9fbee9a36adb057cbf2
deevyagtm <deevyagtm@gmail.com>
2025-07-23 16:50:38 +0545

    frontend about initial page

--- stickify/routes/web.php
+++ stickify/routes/web.php
@@ -1,8 +1,10 @@
+<?php
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TokenController;
 
 Route::get('/dashboard', function () {
     return view('dashboard');
-})->name('dashboard')->middleware('auth'); // ensure user is logged in
+})->name('dashboard'); // ensure user is logged in
 
 Route::post('/generate-token', [TokenController::class, 'generateToken'])
     ->name('generate.token')
